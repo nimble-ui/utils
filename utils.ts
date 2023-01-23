3d3047f8bfaef1087788b1ed0fbcda0922799a59
IRod22 <ivan18rod@yahoo.com>
2023-01-22 19:20:23 -0600

    feat: add `diffNodes()`

--- utils.ts
+++ utils.ts
@@ -16,3 +16,35 @@ export function safe_eq(x: any, y: any) {
     }
     return false
 }
+
+
+
+export function diffNodes<TNode, UNode>(ctx:{
+    areSameNodes: (currentNode: TNode, newNode: UNode) => boolean,
+    createNode: (newNode: UNode) => void,
+    updateeNode: (currentNode: TNode, newNode: UNode) => void,
+    moveNode: (currentNode: TNode) => void,
+    removeNode: (currentNode: TNode) => void,
+}, currentChildren: TNode[], newChildren: UNode[]) {
+    let discard: TNode[] = []
+    for (const item of currentChildren) {
+        if (newChildren.length == 0) {
+            discard = [...discard, item]
+        } else if (!ctx.areSameNodes(item, newChildren[0])) {
+            discard = [...discard, item]
+        } else {
+            ctx.updateeNode(item, newChildren[0])
+            newChildren = newChildren.slice(1)
+        }
+    }
+    for (const item of newChildren) {
+        if (discard.some(discarded => ctx.areSameNodes(discarded, item))) {
+            const idx = discard.findIndex(discarded => ctx.areSameNodes(discarded, item))
+            ctx.moveNode(discard[idx])
+            discard = discard.filter((_, i) => i != idx)
+        } else {
+            ctx.createNode(item)
+        }
+    }
+    discard.forEach(discarded => ctx.removeNode(discarded))
+}
